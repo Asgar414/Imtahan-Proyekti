ca1aa191784d92016a05dc91f2691fad2dfae8de
Asgar414 <asgararifoglu12@gmail.com>
2026-08-26 10:43:47 +0400

    son deyisiklikler

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from gui import GroceryApp
 
-if __name__ == "__main__":
-    root = tk.Tk()
+
+root = tk.Tk()
     
-    try:
-        root.tk.call('tk', 'scaling', 1.5)
-    except:
-        pass
+try:
+    root.tk.call('tk', 'scaling', 1.5)
+except:
+    pass
         
-    app = GroceryApp(root)
-    root.mainloop()
+app = GroceryApp(root)
+root.mainloop()
